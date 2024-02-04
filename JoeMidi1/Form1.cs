092b4b57cbe4ac07a5a5a58ce2c096135808552d
Joseph Weitekamp <jweite@yahoo.com>
2024-02-04 09:49:16 -0500

    Disabling screen sleeping, since my new HP Envy x360 won't wake with a screen tap like my old one did.

--- JoeMidi1/Form1.cs
+++ JoeMidi1/Form1.cs
@@ -10,11 +10,26 @@ using System.Windows.Forms;
 using Midi;
 using Newtonsoft.Json;
 using PdfiumViewer;
+using System.Runtime.InteropServices;
 
 namespace JoeMidi1
 {
     public partial class Form1 : Form
     {
+        [FlagsAttribute]
+        public enum EXECUTION_STATE : uint
+        {
+            ES_AWAYMODE_REQUIRED = 0x00000040,
+            ES_CONTINUOUS = 0x80000000,
+            ES_DISPLAY_REQUIRED = 0x00000002,
+            ES_SYSTEM_REQUIRED = 0x00000001
+            // Legacy flag, should not be used.
+            // ES_USER_PRESENT = 0x00000004
+        }
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+
 
         //**************************************************************************
         // Form Common
@@ -36,6 +51,9 @@ namespace JoeMidi1
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // Prevent Screensaver
+            SetThreadExecutionState(EXECUTION_STATE.ES_DISPLAY_REQUIRED | EXECUTION_STATE.ES_CONTINUOUS);
+
             // this.TopMost = true;
             // this.FormBorderStyle = FormBorderStyle.None;
             this.WindowState = FormWindowState.Maximized;
